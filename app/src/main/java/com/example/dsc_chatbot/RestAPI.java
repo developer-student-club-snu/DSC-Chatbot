c472e5196eb6718b72ca6124d92aedff6eccc285
Divya Raj <divya21raj@gmail.com>
2019-09-12 16:49:33 +0530

    Gson Lenient Added

--- app/src/main/java/com/example/dsc_chatbot/RestAPI.java
+++ app/src/main/java/com/example/dsc_chatbot/RestAPI.java
@@ -1,9 +1,17 @@
 package com.example.dsc_chatbot;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RestAPI {
+
+    static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
     //Creating a retrofit object
     static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(RestService.BASE_URL)
