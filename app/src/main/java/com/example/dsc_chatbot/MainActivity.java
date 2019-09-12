648502e0e125032ae9c1cbbcdbcac8515c638edb
Divya Raj <divya21raj@gmail.com>
2019-09-12 08:00:00 +0530

    Retrofit Added

--- app/src/main/java/com/example/dsc_chatbot/MainActivity.java
+++ app/src/main/java/com/example/dsc_chatbot/MainActivity.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +22,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating a retrofit object
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RestAPI.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()) //Here we are using the GsonConverterFactory to directly convert json data to object
+                .build();
+
+        //creating the api interface
+        RestAPI api = retrofit.create(RestAPI.class);
+
+        //now making the call object
+        //Here we are using the api method that we created inside the api interface
+        Call<List<Map<String, String>>> call = api.getHeroes();
+
+        call.enqueue(new Callback<List<Map<String, String>>>() {
+            @Override
+            public void onResponse(Call<List<Map<String, String>>> call, Response<List<Map<String, String>>> response) {
+                Log.d("Call", response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Map<String, String>>> call, Throwable t) {
+                Log.e("Call", t.toString() );
+            }
+        });
+
     }
 
     public void onButtonPress(View view) {
