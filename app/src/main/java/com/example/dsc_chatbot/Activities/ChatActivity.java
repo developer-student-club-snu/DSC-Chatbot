bdd23ff5943cda0f4b38ea67f3fc1e471afd0df9
Divya Raj <divya21raj@gmail.com>
2019-09-12 19:56:19 +0530

    Recycler View Added

--- app/src/main/java/com/example/dsc_chatbot/Activities/ChatActivity.java
+++ app/src/main/java/com/example/dsc_chatbot/Activities/ChatActivity.java
@@ -11,10 +11,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dsc_chatbot.MyAdapter;
 import com.example.dsc_chatbot.R;
 import com.example.dsc_chatbot.RestAPI;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import nl.dionsegijn.konfetti.KonfettiView;
@@ -29,6 +33,11 @@ public class ChatActivity extends AppCompatActivity {
     EditText messageBox;
     ImageView sendButton;
 
+    private RecyclerView recyclerView;
+    private MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
     private KonfettiView viewKonfetti;
 
     @Override
@@ -40,31 +49,72 @@ public class ChatActivity extends AppCompatActivity {
         sendButton = findViewById(R.id.sendButton);
 
         viewKonfetti = findViewById(R.id.viewKonfetti);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        ArrayList<String> myDataset = new ArrayList<>();
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
     }
 
     public void onSendClick(View view) {
-        String message = messageBox.getText().toString();
+        final String message = messageBox.getText().toString();
+
 
         if(message.toLowerCase().equals("confetti")){
-            viewKonfetti.build()
-                    .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
-                    .setDirection(0.0, 359.0)
-                    .setSpeed(1f, 5f)
-                    .setFadeOutEnabled(true)
-                    .setTimeToLive(2000L)
-                    .addShapes(Shape.RECT, Shape.CIRCLE)
-                    .addSizes(new Size(12, 5))
-                    .setPosition(-50f, viewKonfetti.getWidth() + 50f, -50f, -50f)
-                    .streamFor(300, 5000L);
+            showConfetti();
         }else {
 
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
             Log.d("SEND", "Send was clicked");
+
+            mAdapter.addMessage(message);
+
+            RestAPI.getAppService().getReply(message).enqueue(new Callback<Map<String, String>>() {
+                @Override
+                public void onResponse(Call<Map<String, String>> call, Response<Map<String, String>> response) {
+                    String answer = response.body().get("response");
+                    //Toast.makeText(ChatActivity.this, response.body().get("response"), Toast.LENGTH_SHORT).show();
+                    mAdapter.addMessage(answer);
+                }
+
+                @Override
+                public void onFailure(Call<Map<String, String>> call, Throwable t) {
+                    Toast.makeText(ChatActivity.this, "Network Error!", Toast.LENGTH_SHORT).show();
+                }
+            });
 
         }
         messageBox.setText("");
 
+        //hideKeyboard();
+    }
+
+    private void hideKeyboard() {
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(messageBox.getWindowToken(), 0);
+    }
+
+    private void showConfetti() {
+        viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(new Size(12, 5))
+                .setPosition(-50f, viewKonfetti.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 5000L);
     }
 }
